0644c389247162d96a6d7f0bbd70f449529ff098
Markic01 <01marko.radosavljevic@gmail.com>
2023-12-07 00:25:36 +0100

    ready to defend

--- distorzija/dlt.py
+++ distorzija/dlt.py
@@ -3,27 +3,6 @@ import numpy as np
 
 points = []
 
-def normalize(points):
-    x = sum([point[0] for point in points]) / len(points)
-    y = sum([point[1] for point in points]) / len(points)
-    
-    translated = [[point[0] - x, point[1] - y, point[2]] for point in points]
-    G = np.array([
-        [1, 0, -x],
-        [0, 1, -y],
-        [0, 0, 1]
-    ])
-    
-    avgDist = sum([np.sqrt(point[0]**2+point[1]**2) for point in translated]) / len(points)
-    scale = np.sqrt(2) / avgDist
-    S = np.array([
-        [scale, 0 , 0],
-        [0, scale, 0],
-        [0, 0, 1]
-    ])
-    
-    return np.dot(S,G)
-
 def DLTAlgorithm(points, destPoints):
     A = []
     
@@ -44,9 +23,7 @@ def DLTAlgorithm(points, destPoints):
         
     _, _, V = np.linalg.svd(A, full_matrices= True)
         
-    T = V[-1].reshape(3,3)
-    T = T / T[2][2]
-    return T
+    return V[-1].reshape(3,3)
 
 def calcDist(point1, point2):
     return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
@@ -76,51 +53,29 @@ def getDest(points):
     
     return destPoints
 
-# def onClick(event, x, y, flags, param):
-#     if event  == cv2.EVENT_LBUTTONDOWN:
-#         if len(points) < 4:
-#             points.append([x, y, 1])
+def onClick(event, x, y, flags, param):
+    if event  == cv2.EVENT_LBUTTONDOWN:
+        if len(points) < 4:
+            points.append([x, y, 1])
             
-#             cv2.putText(image, 'X', (x-16,y+14), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 100), 3, cv2.LINE_AA)
-#             cv2.imshow('distorted image', image)
+            cv2.putText(image, 'X', (x-16,y+14), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 100), 3, cv2.LINE_AA)
+            cv2.imshow('distorted image', image)
             
-#         if len(points) == 4:
-#             destPoints = getDest(points)
-#             matrix = DLTAlgorithm(points, destPoints)
-#             out = cv2.warpPerspective(image, matrix, (500,900))
+        if len(points) == 4:
+            destPoints = getDest(points)
+            matrix = DLTAlgorithm(points, destPoints)
+            out = cv2.warpPerspective(image, matrix, (500,900))
             
-#             cv2.imshow('result', out)
-#             cv2.waitKey(0)
+            cv2.imshow('result', out)
+            cv2.waitKey(0)
 
-# path = './3.jpeg'
-# image = cv2.imread(path, cv2.IMREAD_COLOR)
-# image = cv2.resize(image, (500, 900), interpolation = 2)
-# cv2.namedWindow('distorted image')
-# cv2.moveWindow('distorted image', 2440, 250)
-# cv2.imshow('distorted image', image)
-# cv2.setMouseCallback('distorted image', onClick)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# cv2.waitKey(1)
-
-
-def normalizedDLTAlgorithm(points, destPoints):
-    T = normalize(points)
-    Tp = normalize(destPoints)
-
-    M = np.dot(T,np.transpose(points)).transpose()
-    Mp = np.dot(Tp, np.transpose(destPoints)).transpose()
-    
-    Pp = DLTAlgorithm(M, Mp)
-    
-    return np.linalg.inv(Tp).dot(Pp).dot(T)
-
-def DLT(origs, imgs):
-    np.set_printoptions(precision=5, suppress=True)
-    return DLTAlgorithm(origs,imgs)
-
-trapez = [[- 3, - 1, 1], [3, - 1, 1], [1, 1, 1], [- 1, 1, 1], [1,2,3], [-8,-2,1]] 
-pravougaonik1 = [[- 2, - 1, 1], [2, - 1, 1], [2, 1, 1], [- 2, 1, 1], [2,1,5], [-16,-5,5]]
-print(DLT(trapez, pravougaonik1))
-# ako gledate uradjeni primer, primetite [2,1,4]->[2,1,5], [-16,-5,4]->[-16,-5,5]
-# zato matrica nije ista kao u primeru
+path = './3.jpeg'
+image = cv2.imread(path, cv2.IMREAD_COLOR)
+image = cv2.resize(image, (500, 900), interpolation = 2)
+cv2.namedWindow('distorted image')
+cv2.moveWindow('distorted image', 2440, 250)
+cv2.imshow('distorted image', image)
+cv2.setMouseCallback('distorted image', onClick)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.waitKey(1)
--- osmoteme/1.py
+++ osmoteme/1.py
@@ -1,3 +1,11 @@
+import cv2
+path = './slika.jpeg'
+image = cv2.imread(path)
+windowName = 'box'
+cv2.imshow(windowName, image)
+
+corners = []
+
 def cross(a,b):
     x = a[1]*b[2]-a[2]*b[1]
     y = a[2]*b[0]-a[0]*b[2]
@@ -18,8 +26,24 @@ def osmoteme(a):
     
     presek3 = cross(prava5,prava6)
     presek3 = list(map(lambda x: x/presek3[2],presek3))
-    return [int(presek3[0]),int(presek3[1])]
+    print()
+    print('P8: (',int(presek3[0]),', ',int(presek3[1]),')',sep='')
+    return presek3
 
 
+def onClick(event,x,y,flags,data):
+    if event == cv2.EVENT_FLAG_LBUTTON and len(corners) < 7:
+        print('P', len(corners)+1, ': (', x, ', ', y, ')', sep='')
+        cv2.putText(image,'X',(x-15,y+12),cv2.FONT_HERSHEY_PLAIN,3,(0,255,100),3,cv2.LINE_AA)
+        cv2.imshow('image',image)
+        corners.append([x,y])
+        if len(corners) == 7:
+            teme = osmoteme(corners)
+            x = int(teme[0])
+            y = int(teme[1])
+            cv2.putText(image,'X',(x-15,y+12),cv2.FONT_HERSHEY_PLAIN,3,(0,0,225),3,cv2.LINE_AA)
+            cv2.imshow('image',image)
 
-print(osmoteme([[32, 70], [195, 144], [195, 538], [30, 307], [251, 40], [454, 78], [455, 337]]))
+cv2.setMouseCallback(windowName, onClick)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
